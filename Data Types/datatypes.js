228a3a8f0120a67ff43b8927d338d67f6a104e06
Harsh Khare <projection2006@gmail.com>
2025-05-01 09:40:12 +0530

    Data Types

---
+++ Data Types/datatypes.js
@@ -0,0 +1,18 @@
+var age = 12 //-ini 21,321,0,12  +ini
+console.log(age)
+var balance = 32.132
+console.log(typeof(balance))
+var mobileNumber = 23489749242n
+console.log(typeof(mobileNumber))
+var isAllowed = false
+console.log(typeof(true))
+var name = "Avnish" 
+console.log(typeof(name))
+var check = undefined;
+console.log(check);
+console.log(typeof(check));
+var ones = null
+console.log(null)
+var abc = Symbol("abc")
+console.log(abc)
+
