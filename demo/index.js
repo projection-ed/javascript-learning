a210f14c93c35b98e1c0c28d5ee8f0d62a11748d
Harsh Khare <projection2006@gmail.com>
2025-05-01 08:53:47 +0530

    Initial Commit

---
+++ demo/index.js
@@ -0,0 +1,32 @@
+var name = "ABC"
+console.log(name)
+name = "XYZ"
+console.log(name)
+var name = "PQR"
+console.log(name)
+
+let email;
+// email = "zya@gmail.com"
+console.log(email)
+
+let result = email + ""
+console.log(result)
+
+const PI = 3.14;
+// PI = 3.313
+console.log(PI)
+
+
+password = "abkjfkja"
+console.log(password)
+
+/*
+1. A-Z
+2. _$
+3. A729853293526395
+4. Vaild Name
+5. user name
+username
+UserName  // Pascal Case
+userName // Camel Case
+*/
