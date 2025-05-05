bd7c93c2d526b45f3d62032f000ae6417b0f711e
Harsh Khare <projection2006@gmail.com>
2025-05-05 09:39:05 +0530

    Operators

---
+++ Operators/Comparission.js
@@ -0,0 +1,16 @@
+// < > <= >= != == ===
+
+let a = 30
+let b = 50
+console.log(a < b) // true
+console.log(a > b) // false
+console.log(a <= b) // true
+console.log(a >= b) // false
+console.log(a != b) // true
+console.log(a == b) // false
+console.log(a === b) // false
+
+let s = "20"
+let k = 20
+console.log(s == k)
+console.log(s === k)
---
+++ Operators/Logical.js
@@ -0,0 +1,19 @@
+// || && !^
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+console.log(true ^ true)
+console.log(true ^ false)
+console.log(false ^ true)
+console.log(false ^ false)
+
+console.log(!true)
+console.log(!false)
