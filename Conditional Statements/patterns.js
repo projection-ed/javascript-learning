ab0654a2d9028039126b0cc3948340dfa100fc91
Harsh Khare <projection2006@gmail.com>
2025-05-15 09:28:06 +0530

    Star Pattern

--- Conditional Statements/patterns.js
+++ Conditional Statements/patterns.js
@@ -8,8 +8,8 @@ let n = 5
 
 var star = ""
 for(let i = 1;i<=n;i++){
-    for(let j = 1;j<=n;j++){
-        if(j>=i && j<=5){
+    for(let j = 1;j<2*n;j++){
+        if((n-i+1)<=j && j<= (n+i-1)){
             star += "* "
         }else{
             star += "  "
