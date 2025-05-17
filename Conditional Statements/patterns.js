647fee71546200490e9388c174658630b745b819
Harsh Khare <projection2006@gmail.com>
2025-05-17 09:32:26 +0530

    Patterns

--- Conditional Statements/patterns.js
+++ Conditional Statements/patterns.js
@@ -7,12 +7,20 @@
 let n = 5
 
 var star = ""
+
 for(let i = 1;i<=n;i++){
+    var k = 5
     for(let j = 1;j<2*n;j++){
-        if((n-i+1)<=j && j<= (n+i-1)){
-            star += "* "
+        if(j>=(n-i+1) && j<=(n+i-1)){
+            star += k + " "
         }else{
             star += "  "
+        }
+
+        if(j<n){
+            k--
+        }else{
+            k++
         }
     }
     star += "\n"
