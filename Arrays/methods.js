9836db1e064057c1c74b4ebf9a033ded6421b64d
Harsh Khare <projection2006@gmail.com>
2025-05-20 09:29:40 +0530

    methods

---
+++ Arrays/methods.js
@@ -0,0 +1,25 @@
+var arr1 = [1,2,3,4,6]
+var arr2 = "#"
+
+// console.log(arr1.join(arr2))
+console.log(arr1.push(10))
+console.log(arr1)
+var res =  arr1.pop()
+console.log(res)
+
+console.log(arr1.shift())
+console.log(arr1.unshift(30))
+arr1[2] = "Harsh"
+arr1[1] = 2.4
+arr1[4] = true
+console.log(arr1.delete)
+console.log(arr1)
+
+const names = ["Joy","Allen","Rock","Dogesh","Iron"]
+names.push("ABC")
+var ratings = [1.2,3,4.5,7.4,10,5]
+console.log(names.concat(ratings))
+console.log(names)
+
+[21,3,4,5,6,87,4,3,1]
+//
