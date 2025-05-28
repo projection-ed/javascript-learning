fef8c6a4cbd2b8dc49f5ab798bd28bf1c46ef8ce
Harsh Khare <projection2006@gmail.com>
2025-05-28 09:30:47 +0530

    Intro about functions

---
+++ Functions/intro.js
@@ -0,0 +1,48 @@
+
+{
+    // Block
+    // Multiple Statements
+} // -> Specific Tasks - Perform
+// Reusablility
+
+// No args no return
+// No args but return
+// Args but no return
+// Args and return
+
+// -> Parameters
+// -> Arguments
+// -> Return
+
+// Function Defination
+function run(){
+    console.log("hello world");
+}
+
+function noArgsReturn(){
+    return "hello world";
+}
+
+function addTwoNumbers(a,b){
+    let c = a + b
+    console.log(c);
+}
+
+function minusTwoNumbers(a,b){
+    let c = a - b
+    return c
+}
+
+//function run(); Declaration
+
+//Function Calling
+run()
+addTwoNumbers(5,8)
+let result = minusTwoNumbers(5,8)
+console.log(result);
+
+"Hello I am in projection"// reverse(s) // projection in am I Hello
+// abba -> abba //Palindrome
+// abcd -> toUpperCase -> ABCD
+// abcd -> toLowerCase ->
+// "I am study in projection" -> 
